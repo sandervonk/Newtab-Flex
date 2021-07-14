0d53dfb268e0bae4313bd53fea7c5468aa542124
sander <sander.c.vonk@gmail.com>
2021-07-14 12:13:20 -0700

    same as prev

--- newtab.js
+++ newtab.js
@@ -1,43 +1,55 @@
 function pad2(number) {
-    return (number < 10 ? '0' : '') + number
+  return (number < 10 ? "0" : "") + number;
+}
+function check12(hours) {
+  if (hours === 0) {
+    hours = 12;
+  }
+  return hours;
 }
 function iframestyle() {
-    var element = document.querySelector("#main > div > div > div.ai.au.ah.av > div.ah.aw.ax.ay.az.b0.b1.b2 > div > div")
-    element.style = "background: black !important;"
-    var frm = document.getElementsByTagName('iframe')[0].document;
-    var otherhead = frm.getElementsByTagName("head")[0];
-    var link = frm.createElement("link");
-    link.setAttribute("rel", "stylesheet");
-    link.setAttribute("type", "text/css");
-    link.setAttribute("href", "style.css");
-    otherhead.appendChild(link);
+  var element = document.querySelector(
+    "#main > div > div > div.ai.au.ah.av > div.ah.aw.ax.ay.az.b0.b1.b2 > div > div"
+  );
+  element.style = "background: black !important;";
+  var frm = document.getElementsByTagName("iframe")[0].document;
+  var otherhead = frm.getElementsByTagName("head")[0];
+  var link = frm.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  link.setAttribute("href", "style.css");
+  otherhead.appendChild(link);
 }
 function setIFrame() {
-    document.getElementsByTagName("iframe")[0].onready = iframestyle
-    document.getElementsByTagName("iframe")[0].onload = iframestyle
-    setTimeout(iframestyle, 3000)
-    console.log("setting onload")
+  document.getElementsByTagName("iframe")[0].onready = iframestyle;
+  document.getElementsByTagName("iframe")[0].onload = iframestyle;
+  setTimeout(iframestyle, 3000);
+  console.log("setting onload");
 }
 function setTime() {
-    time = new Date
-    document.getElementsByClassName("time-align")[0].textContent = time.getHours() % 12 + ":"+pad2(time.getMinutes())
-    var sheets = document.styleSheets
-    if (time.getHours() >=12) {
-        sheets[0].addRule(".flex-box .time-align::after", "content: 'PM' !important;")
-    }
-    setIFrame()
+  time = new Date();
+  document.getElementsByClassName("time-align")[0].textContent =
+    check12(time.getHours() % 12) + ":" + pad2(time.getMinutes());
+  var sheets = document.styleSheets;
+  if (time.getHours() >= 12) {
+    sheets[0].addRule(
+      ".flex-box .time-align::after",
+      "content: 'PM' !important;"
+    );
+  }
+  setIFrame();
 }
-    
-window.onload = setTime
-setTimeout(checkMinutes,1000);
 
-function checkMinutes(){
+window.onload = setTime;
+setTimeout(checkMinutes, 1000);
+
+function checkMinutes() {
   var now = new Date().getMinutes();
-  if (now > checkMinutes.prevTime){
+  if (now > checkMinutes.prevTime) {
     // do something
-    setTime()
+    setTime();
   }
   checkMinutes.prevTime = now;
 }
 
-  setTimeout(checkMinutes,1000);
+setTimeout(checkMinutes, 1000);
